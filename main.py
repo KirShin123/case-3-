5f3b6243a9cd6faa8bb15beb6d18b6193c9ff498
konuxxd <jhjhjhjhjhjhjhjhjjhjhjhjhjhjhj@gmail.com>
2024-12-20 16:15:23 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 
 from Functions import change_document_style
+def main():
+    files_to_process = [
+    'content/1.docx',
+    'content/2.docx',
+    'content/3.docx',
+    'content/4.docx',
+    'content/5.docx'
+    ]
 
-files_to_process = [
-'content/1.docx',
-'content/2.docx',
-'content/3.docx',
-'content/4.docx',
-'content/5.docx'
-]
+    for file_path in files_to_process:
+        print(f'Выводит текстовые файлы» {file_path}')
+        change_document_style(file_path) 
+        
 
-for file_path in files_to_process:
-    print(f'Выводит текстовые файлы» {file_path}')
-    change_document_style(file_path) 
-    
+# Инициализационный скрипт
+if __name__ == "__main__":
+    main()
